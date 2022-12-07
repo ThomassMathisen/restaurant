c0996eb4619516fc4158b2e9250a8709e62f3837
ThomassMathisen <thomasmathisen@hotmail.com>
2022-12-07 15:08:04 +0100

    Create footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,55 @@
+import Image from "next/image";
+import styles from "../styles/Footer.module.css";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <Image src="/img/bg.png" alt="" objectFit="cover" layout="fill" />
+      </div>
+      <div className={styles.item}>
+        <div className={styles.card}>
+          <h2 className={styles.motto}>
+            Problems Come and Go. Pizza is Forever.
+          </h2>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>FIND OUR RESTAURANTS</h1>
+          <p className={styles.text}>
+            1234 5th Avenue
+            <br /> NewYork, 12345
+            <br /> (212) 123 4560
+          </p>
+          <p className={styles.text}>
+            1234 5th Avenue
+            <br /> NewYork, 12345
+            <br /> (212) 123 4560
+          </p>
+          <p className={styles.text}>
+            1234 5th Avenue
+            <br /> NewYork, 12345
+            <br /> (212) 123 4560
+          </p>
+          <p className={styles.text}>
+            1234 5th Avenue
+            <br /> NewYork, 12345
+            <br /> (212) 123 4560
+          </p>
+        </div>
+        <div className={styles.card}>
+          <h1 className={styles.title}>OPENING HOURS</h1>
+          <p className={styles.text}>
+            MONDAY - THURSDAY
+            <br /> 9:00 – 22:00
+          </p>
+          <p className={styles.text}>
+            Friday - SUNDAY
+            <br /> 12:00 – 24:00
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
