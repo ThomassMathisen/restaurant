01687b79a377f8961ace18692e64605d06eb90d0
ThomassMathisen <thomasmathisen@hotmail.com>
2022-12-07 14:51:51 +0100

    Create pizza components

---
+++ components/PizzaCard.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "../styles/PizzaCard.module.css";
+
+const PizzaCard = () => {
+  return (
+    <div className={styles.container}>
+      <Image src="/img/pizza.png" alt="" width="200" height="200" />
+      <h1 className={styles.title}>DIAVOLO</h1>
+      <span className={styles.price}>$14.99</span>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
+  );
+};
+
+export default PizzaCard;
