f75d2efe56df2cc94e36859c09641ba1bb295d7f
ThomassMathisen <thomasmathisen@hotmail.com>
2022-12-08 16:58:22 +0100

    Setup api connection for products

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import styles from "../styles/PizzaCard.module.css";
+import Link from "next/link";
 
-const PizzaCard = () => {
+const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
-      <Image src="/img/pizza.png" alt="" width="200" height="200" />
-      <h1 className={styles.title}>DIAVOLO</h1>
-      <span className={styles.price}>$14.99</span>
-      <p className={styles.desc}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
+      <Link href={`/product/${pizza._id}`} passHref>
+        <Image src={pizza.img} alt="" width="200" height="200" />
+      </Link>
+      <h1 className={styles.title}>{pizza.title}</h1>
+      <span className={styles.price}>${pizza.prices[0]}</span>
+      <p className={styles.desc}>{pizza.desc}</p>
     </div>
   );
 };
--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const ProductSchema = new Schema(
+const ProductSchema = new mongoose.Schema(
   {
     title: {
       type: String,
